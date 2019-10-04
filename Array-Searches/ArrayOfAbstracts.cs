c777e7587c89020a42d11414a0a8c313feee62f9
Pernell Robinson <probinso2017@students.curry.edu>
2019-10-04 15:13:35 -0400

    Finished implementing the nodes coded the rest of the classes.

--- Array-Searches/ArrayOfAbstracts.cs
+++ Array-Searches/ArrayOfAbstracts.cs
@@ -4,10 +4,61 @@ using System.Text;
 
 namespace Array_Searches
 {
-    class ArrayOfAbstracts
+    class ArrayOfAbstracts 
     {
+        private Nodes[] arrayOfNodes;
+        public ArrayOfAbstracts()
+        {
 
 
+        }
 
+        public Nodes[] arrayWithNodes()
+        {
+            for(int a = 0; a < arrayOfNodes.Length; a++)
+            {
+                Nodes temp = new Nodes();
+
+                Console.WriteLine();
+                temp.firstName = Console.ReadLine();
+
+                arrayOfNodes[a] = temp;
+            }
+
+            return arrayOfNodes;
+        }
+
+        public void numberOfElements()
+        {
+            int count = 0;
+
+            for (int a = 0; a < arrayOfNodes.Length; a++)
+            {
+                count++;
+
+            }
+        }
+
+        public void displayValues()
+        {
+            for (int a = 0; a < arrayOfNodes.Length; a++)
+            {
+                Console.WriteLine(arrayOfNodes[a]);
+            }
+        }
+
+        public void searhForWord(Nodes searchNumber)
+        {
+            for (int a = 0; a < arrayOfNodes.Length; a++)
+            {
+                if (searchNumber == arrayOfNodes[a])
+                {
+                    Console.WriteLine(searchNumber + " is in the list of arrayOfNodes");
+                    return;
+                }
+            }
+
+            Console.WriteLine(searchNumber + " is not in this list");
+        }
     }
 }
---
+++ Array-Searches/Nodes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Array_Searches
+{
+    class Nodes
+    {
+
+        public string firstName;
+    }
+}
